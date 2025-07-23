7e1f0db92ecd79c750ccf69f4786123b1299f9db
chen <alf138540fun@gmail.com>
2025-07-23 09:15:54 +0200

    Add uid into cookie

--- server/settings.py
+++ server/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.user.middleware.UserUidMiddleware'
 ]
 
 ROOT_URLCONF = 'server.urls'
