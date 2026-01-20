3b41aa4d89109cad3ce78d35c3e1cabec1db6f4a
Chen <chensong64@h-partners.com>
2026-01-20 14:03:48 +0100

    Archive project architecture

--- archived-docs/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "c:\Users\alf13\Documents\pyrepos\strayPet\frontend"
-call npm run build:pro
-cd /d "c:\Users\alf13\Documents\pyrepos\strayPet\backend"
-docker-compose restart web
--- archived-docs/run_test.ps1
+++
@@ -1,3 +0,0 @@
-cd c:\Users\alf13\Documents\pyrepos\strayPet\backend
-$content = Get-Content test_friend_request.py -Raw
-python manage.py shell -c "$content"
